5e3e037b0ed59e5cc31a0af4afa39b2a6c5c5249
tarun reddy <tarunreddy213@yahoo.com>
2020-06-03 15:35:29 +0530

    Added Dashboard Stored Procedure to Db

--- LoMan/Controllers/HomeController.cs
+++ LoMan/Controllers/HomeController.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LoMan.Models;
+using LoMan.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoMan.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            _ = _db.Dashboard.FromSqlRaw("EXECUTE dbo.Set_Dashboard").ToList();
+           return View(_db.Dashboard.FirstOrDefault());
         }
 
         public IActionResult Privacy()
--- LoMan/Models/Dashboard.cs
+++ LoMan/Models/Dashboard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,5 +20,6 @@ namespace LoMan.Models
         public int Pincrement { get; set; }
         public int Iincrement { get; set; }
         public int MonthlyGrowth { get; set; }
+
     }
 }
